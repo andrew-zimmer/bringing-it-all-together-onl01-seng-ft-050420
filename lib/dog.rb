808ab570932c4544b18de15a309f8d706c0aaf97
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-06-04 14:41:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,6 +65,13 @@ class Dog
     self.new_from_db(DB[:conn].execute(sql, id)[0])
   end 
   
+  def self.find_or_create_by(name, breed) 
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed) 
+    if !dog.empty?
+      dog_data = dog[0]
+      self.new_from_db(dog_data)
+  end 
+  
   
   
   
